57bdd75af79efff8b864136eafa164d78220bf6f
miyatsuki <miyatsuki.shiku@gmail.com>
2021-10-31 22:59:19 +0900

    関数名被ってる

--- api/index.py
+++ api/index.py
@@ -6,13 +6,13 @@ def catch1(path):
     return Response("<h1>Flask</h1><p>You visited:test1", mimetype="text/html")
 
 @app.route('/api/test2')
-def catch1(path):
+def catch2(path):
     return Response("<h1>Flask</h1><p>You visited:test2", mimetype="text/html")
 
 @app.route('/api/test3')
-def catch1(path):
+def catch3(path):
     return Response("<h1>Flask</h1><p>You visited:test3", mimetype="text/html")
 
 @app.route('/api/test4')
-def catch1(path):
+def catch4(path):
     return Response("<h1>Flask</h1><p>You visited:test4", mimetype="text/html")
