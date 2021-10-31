30edf3147458ad9e1651249e3ca939699261d506
miyatsuki <miyatsuki.shiku@gmail.com>
2021-10-31 17:53:05 +0900

    flaskやめる

--- api/index.py
+++ api/index.py
@@ -1,7 +1,16 @@
-from flask import Flask, Response
-app = Flask(__name__)
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        if "name" in dic:
+            message = "Hello, " + dic["name"] + "!"
+        else:
+            message = "Hello, stranger!"
+        self.wfile.write(message.encode())
+        return
