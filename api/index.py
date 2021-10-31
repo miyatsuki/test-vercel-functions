fe539e5da670ac9b19a41a2d0f10fe8ce896534c
miyatsuki <miyatsuki.shiku@gmail.com>
2021-10-31 23:01:52 +0900

    わかりにくかったので直す

--- api/index.py
+++ api/index.py
@@ -1,18 +1,12 @@
 from flask import Flask, Response
 app = Flask(__name__)
 
-@app.route('/api/test1')
-def catch1(path):
-    return Response("<h1>Flask</h1><p>You visited:test1", mimetype="text/html")
+@app.route('/')
+def hello():
+    name = "Hello World"
+    return name
 
-@app.route('/api/test2')
-def catch2(path):
-    return Response("<h1>Flask</h1><p>You visited:test2", mimetype="text/html")
-
-@app.route('/api/test3')
-def catch3(path):
-    return Response("<h1>Flask</h1><p>You visited:test3", mimetype="text/html")
-
-@app.route('/api/test4')
-def catch4(path):
-    return Response("<h1>Flask</h1><p>You visited:test4", mimetype="text/html")
+@app.route('/good')
+def good():
+    name = "Good"
+    return name
